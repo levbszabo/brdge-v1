af587e4a6583e0096590a19cf693b99a3cce593f
levbszabo <levbszabo@gmail.com>
2024-11-11 09:43:23 -0500

    removing config

--- frontend/src/config.js
+++
@@ -1,4 +0,0 @@
-// src/config.js
-export const S3_BUCKET_URL = 'https://brdges.s3.us-east-1.amazonaws.com';
-export const BACKEND_URL = "http://localhost:5000/api"
-//export const BACKEND_URL = "/api"; // Ensure this is correctly set based on your deployment
