fbb8d74a7d9f108c6487355692f6c62fada53690
levbszabo <levbszabo@gmail.com>
2024-11-19 14:31:07 -0500

    adding

--- frontend/src/pages/EditBrdgePage.jsx
+++ frontend/src/pages/EditBrdgePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import AgentsPlayground from '../components/AgentsPlayground';
 
@@ -7,24 +7,13 @@ function EditBrdgePage() {
     const { id } = useParams();
 
     return (
-        <Container maxWidth="xl">
-            <Box sx={{ py: 4 }}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Edit Brdge
-                </Typography>
-                <Box
-                    sx={{
-                        height: 'calc(100vh - 200px)',
-                        border: '1px solid #ccc',
-                        borderRadius: 2,
-                        bgcolor: 'background.paper',
-                        overflow: 'hidden'
-                    }}
-                >
-                    <AgentsPlayground brdgeId={id} />
-                </Box>
-            </Box>
-        </Container>
+        <Box sx={{
+            height: '100vh',
+            width: '100vw',
+            overflow: 'hidden'  // Prevent any scrolling at container level
+        }}>
+            <AgentsPlayground brdgeId={id} />
+        </Box>
     );
 }
 
