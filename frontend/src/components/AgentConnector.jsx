d2f6407068217412cb89298c4c946ce0f09b8534
levbszabo <levbszabo@gmail.com>
2025-01-22 03:48:38 -0500

    new fix

--- frontend/src/components/AgentConnector.jsx
+++ frontend/src/components/AgentConnector.jsx
@@ -80,11 +80,19 @@ function AgentConnector({ brdgeId, agentType = 'edit', token }) {
             sx={{
                 width: '100%',
                 height: '100%',
+                overflow: 'hidden',
+                position: 'relative',
                 '& > iframe': {
                     width: '100%',
                     height: '100%',
                     border: 'none',
-                    backgroundColor: 'transparent'
+                    backgroundColor: 'transparent',
+                    overflow: 'hidden',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0
                 }
             }}
         >
